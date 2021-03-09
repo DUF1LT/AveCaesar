30ce6a9053065d8aaa025a546d8a140429c03f1b
Vadim <duf1lt@yandex.ru>
2021-03-09 23:15:55 +0300

    Created AuthorizationWindow. Added Logo and SubText with adaptive properties.

--- AveCaesarApp/AuthorizationWindow.xaml.cs
+++ AveCaesarApp/AuthorizationWindow.xaml.cs
@@ -22,5 +22,33 @@ namespace AveCaesarApp
         {
             InitializeComponent();
         }
+
+        private void SubTextBox_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+            if (Height > 300 && Height <= 600)
+            {
+                SubTextBox.FontSize = 18;
+                SubTextBox.MaxWidth = 350;
+            }
+
+            if (Height > 600 && Height <= 800)
+            {
+                SubTextBox.MaxWidth = 450;
+                SubTextBox.FontSize = 23;
+            }
+
+            if (Height > 800)
+            {
+                SubTextBox.FontSize = 30;
+                SubTextBox.MaxWidth = 600;
+
+            }
+            if (this.WindowState == WindowState.Maximized)
+            {
+                SubTextBox.FontSize = 35;
+                SubTextBox.MaxWidth = 600;
+            }
+        }
     }
 }
