21316da6c0fa3f694dc341176e4fb997cab309ba
Vadim <duf1lt@yandex.ru>
2021-04-21 14:23:39 +0300

    Created ProductsListToString converter to display list of products at dish Viewtem

---
+++ AveCaesarApp/Converters/ProductsListToStringConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+using AveCaesarApp.Models;
+
+namespace AveCaesarApp.Converters
+{
+    class ProductsListToStringConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+           return this;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is IList productsList)
+            {
+                string result = string.Empty;
+                foreach (Product product in productsList)
+                {
+                    result += product.Name.ToLower() + ", ";
+                }
+
+                result = result.Remove(result.Length - 2, 2);
+                return result;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
